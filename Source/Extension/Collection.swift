f9179ebbc8385d36bb7199ed0ab7389147913d89
Dark Dong <viva@163.com>
2017-08-07 14:26:10 +0800

    Remove Array.init(json:)

--- Source/Extension/Collection.swift
+++ Source/Extension/Collection.swift
@@ -33,19 +33,6 @@ public extension Sequence {
 }
 
 public extension Array {
-    init(json: Any?, constructor: (Any?) -> Element?) {
-        self.init()
-        
-        if let jsonArray = json as? [Any?] {
-            reserveCapacity(jsonArray.count)
-            for json in jsonArray {
-                if let element = constructor(json) {
-                    append(element)
-                }
-            }
-        }
-    }
-    
     var randomIndex: Int {
         return Int(arc4random_uniform(UInt32(count)))
     }
